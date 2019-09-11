70673aad3ebc648404d86fec793bca2d6d89f1e7
José Romildo Malaquias <malaquias@gmail.com>
2019-09-11 12:55:21 -0300

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {} } :
+
+let
+  inherit (nixpkgs) pkgs;
+  ocamlPackages = pkgs.ocamlPackages_latest;
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "my-ocaml-env-0";
+  buildInputs = [
+    ocamlPackages.dune
+    ocamlPackages.findlib
+    ocamlPackages.menhir
+    ocamlPackages.merlin
+    ocamlPackages.ocaml
+    ocamlPackages.patience_diff
+    ocamlPackages.ppx_deriving
+    ocamlPackages.ppx_expect
+    ocamlPackages.ppx_here
+    ocamlPackages.ppx_import
+    ocamlPackages.re
+    ocamlPackages.utop
+    pkgs.ocamlformat
+    pkgs.rlwrap
+    pkgs.vscode
+    (pkgs.emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
+      pkgs.dune
+      pkgs.ocamlformat
+    ])))
+  ];
+}
